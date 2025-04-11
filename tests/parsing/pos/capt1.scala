224b1334a27c85a68524f462916e487f380ead85
Yichen Xu <yichen.x@outlook.com>
2025-04-12 00:18:21 +0200

    add capt parsing tests

---
+++ tests/parsing/pos/capt1.scala
@@ -0,0 +1,2 @@
+val id: (x: IO^{cap}) -> IO^{x} = (x: IO^{cap}) => x
+val xs: List[IO^{io}] = test
---
+++ tests/parsing/pos/capt2.scala
@@ -0,0 +1,2 @@
+val foo: List[IO^{io}]^{io} = test
+val bar: List[IO^{io}, [cap C] -> U^{C}]^{io} = test
