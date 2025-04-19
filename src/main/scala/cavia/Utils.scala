20e68ba0b9aecbf8fabd7bbb5e4d34998285909c
Yichen Xu <yichen.x@outlook.com>
2025-04-20 01:43:14 +0200

    add util for direct-style error handling

---
+++ src/main/scala/cavia/Utils.scala
@@ -0,0 +1,12 @@
+package cavia
+
+import scala.util.boundary, boundary.Label, boundary.break
+
+def result[E, X, R](op: Label[Either[E, X]] ?=> X): Either[E, X] = boundary:
+  Right(op)
+
+extension [E, X](mx: Either[E, X])
+  def !![R](using l: Label[Either[E, R]]): X = mx match
+    case Left(err) => break(Left(err))
+    case Right(value) => value
+  
