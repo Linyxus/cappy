6c2de38ba6a0a2f8d945df3afcdde037eeb50c26
Yichen Xu <yichen.x@outlook.com>
2025-04-20 19:07:07 +0200

    tons of tests and checkfiles

--- src/test/scala/TypeCheckingSuite.scala
+++ src/test/scala/TypeCheckingSuite.scala
@@ -1,18 +1,47 @@
 package cavia
+import typechecking.*
+import java.nio.file.*
+import cavia.reporting.Printer
 
 class TypeCheckingSuite extends munit.FunSuite:
-  for source <- loadAllSourceFiles("tests/pos") do
-    test(s"pos typechecking test: ${source.name}"):
-      val parsed = Compiler.parse(source)
+  def writeOutput(content: String, path: Path): Unit =
+    if Files.exists(path) then
+      Files.delete(path)
+    Files.createFile(path)
+    Files.writeString(path, content)
+
+  for testCase <- loadAllTestcases("tests/pos") do
+    test(s"pos typechecking test: ${testCase.path.getFileName}"):
+      val parsed = Compiler.parse(testCase.source)
       assert(parsed.isInstanceOf[Compiler.ParseResult.Ok])
       val defs = parsed.asInstanceOf[Compiler.ParseResult.Ok].result
       val typedModule = Compiler.typecheck(defs)
       assert(typedModule.isRight)
+      val module = typedModule.getOrElse(???)
+      if testCase.checkFile.isDefined then
+        val expected = testCase.checkFile.get
+        val actualStr = ExprPrinter.show(module)(using TypeChecker.Context.empty)
+        val outputPath = testCase.path.resolveSibling(testCase.path.getFileName.toString.replace(".scala", ".actual"))
+        val expectedPath = testCase.path.resolveSibling(testCase.path.getFileName.toString.replace(".scala", ".check"))
+        writeOutput(actualStr, outputPath)
+        val hintStr = s"To overwrite the expected output, run\n\n  mv ${outputPath.toString} ${expectedPath.toString}\n\n"
+        assertEquals(actualStr.strip, expected.strip, hintStr)
 
-  for source <- loadAllSourceFiles("tests/neg") do
-    test(s"neg typechecking test: ${source.name}"):
-      val parsed = Compiler.parse(source)
+  for testCase <- loadAllTestcases("tests/neg") do
+    test(s"neg typechecking test: ${testCase.path.getFileName}"):
+      val parsed = Compiler.parse(testCase.source)
       assert(parsed.isInstanceOf[Compiler.ParseResult.Ok])
       val defs = parsed.asInstanceOf[Compiler.ParseResult.Ok].result
-      val typedModule = Compiler.typecheck(defs)
-      assert(typedModule.isLeft)
+      Compiler.typecheck(defs) match
+        case Left(err) => 
+          testCase.checkFile match
+            case None =>
+            case Some(expectedStr) =>
+              val actualStr = Printer.showSourcePos(err.pos, List(err.toString))
+              val outputPath = testCase.path.resolveSibling(testCase.path.getFileName.toString.replace(".scala", ".actual"))
+              val expectedPath = testCase.path.resolveSibling(testCase.path.getFileName.toString.replace(".scala", ".check"))
+              writeOutput(actualStr, outputPath)
+              val hintStr = s"To overwrite the expected output, run\n\n  mv ${outputPath.toString} ${expectedPath.toString}\n\n"
+              assertEquals(actualStr.strip, expectedStr.strip, hintStr)
+        case Right(module) =>
+          fail(s"Typechecking should have failed for ${testCase.path.getFileName}")
--- src/test/scala/Utils.scala
+++ src/test/scala/Utils.scala
@@ -3,6 +3,8 @@ import io.*
 import java.nio.file.{Files, Path, Paths}
 import scala.jdk.CollectionConverters.*
 
+case class Testcase(path: Path, source: SourceFile, checkFile: Option[String])
+
 def loadAllSourceFiles(dir: String): List[SourceFile] =
   val path = Paths.get(dir)
   if !Files.exists(path) then
@@ -13,4 +15,24 @@ def loadAllSourceFiles(dir: String): List[SourceFile] =
     .asScala
     .filter(p => !Files.isDirectory(p) && p.toString.endsWith(".scala"))
     .map(p => SourceFile.fromPath(p))
+    .toList
+
+/** Load all testcases in the given directory, along with their expected output. */
+def loadAllTestcases(dir: String): List[Testcase] =
+  val path = Paths.get(dir)
+  if !Files.exists(path) then
+    return List.empty
+
+  def retrieveCheckFile(path: Path): Option[String] =
+    val checkPath = path.resolveSibling(path.getFileName.toString.replace(".scala", ".check"))
+    if Files.exists(checkPath) then
+      Some(SourceFile.fromPath(checkPath).content)
+    else
+      None
+
+  Files.walk(path)
+    .iterator()
+    .asScala
+    .filter(p => !Files.isDirectory(p) && p.toString.endsWith(".scala"))
+    .map(p => Testcase(p, SourceFile.fromPath(p), retrieveCheckFile(p)))
     .toList
