057962f9e8173b32b3590c3fd52b47fabb133a6a
Yichen Xu <yichen.x@outlook.com>
2025-04-20 03:16:34 +0200

    enrich the example

--- tests/tapp1.scala
+++ tests/tapp1.scala
@@ -2,4 +2,7 @@ def test(): Unit =
   val f1 = [X] => (x: X) => x
   val t2 = f1[i64](0)
   val f3 = [X, Y <: X] => (x: Y) => x
-  val t4 = f3[i64, Int](0)
+  val t4 = f3[i64, i64](0)
+  val f5 = [X, Y <: X, Z <: X] => 0
+  val t6 = f5[i32, i32, i32]
+  val t7 = f5[i32, i32, i64]
