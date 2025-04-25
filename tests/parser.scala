9149753d1248149e8acf375d3a52a4760c0bfd6d
Yichen Xu <yichen.x@outlook.com>
2025-04-25 17:58:40 +0200

    tweak the example

--- tests/parser.scala
+++ tests/parser.scala
@@ -1,4 +1,6 @@
-struct ParserInfo(what: array[i32])
-struct Parser(runParser: () => Unit, info: ParserInfo)
+struct ParserInfo(what: array[i32]^)
+struct Parser(runParser: () => Unit, info: ParserInfo^)
 def main(): Unit =
+  val p = Parser(() => (), ParserInfo(newArray[i32](10, 0)))
+  p.runParser()
   ()
