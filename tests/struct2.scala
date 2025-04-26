81b5ba6a5ba1697702ed59aca724e87688600d33
Yichen Xu <yichen.x@outlook.com>
2025-04-26 15:35:25 +0200

    change struct2 example

--- tests/struct2.scala
+++ tests/struct2.scala
@@ -1,18 +1,22 @@
 struct Pair[A, B](var x: A, var y: B)
+def iterate(size: i32, op: i32 => Unit): Unit =
+  def go(cur: i32): Unit =
+    if cur < size then
+      op(cur)
+      go(cur+1)
+  go(0)
+def showArray(xs: array[i32]^): Unit =
+  iterate(xs.size, (idx: i32) => #i32println(xs(idx)))
+def mapArray(xs: array[i32]^, f: i32 => i32): Unit =
+  def go(cur: i32): Unit =
+    if cur < xs.size then
+      xs(cur) = f(xs(cur))
+      go(cur+1)
+  go(0)
 def main(): Unit =
   val p = Pair[i32, array[i32]^](0, newArray[i32](10, 42))
-  #i32println(p.x)
   p.x = 42
-  #i32println(p.x)
-  def go(cur: i32): Unit =
-    if cur < p.y.size then
-      p.y(cur) = 100
-      go(cur+1)
-  go(0)
-  def go(cur: i32): Unit =
-    if cur < p.y.size then
-      p.x = p.x + p.y(cur)
-      #i32println(p.y(cur))
-      go(cur+1)
-  go(0)
-  #i32println(p.x)
+  iterate(p.y.size, (idx: i32) => p.y(idx) = 100)
+  showArray(p.y)
+  mapArray(p.y, (x: i32) => x + 1)
+  showArray(p.y)
