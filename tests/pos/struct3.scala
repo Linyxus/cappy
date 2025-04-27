89bc07b2e31740a354a2f996f30ab37ae3385bee
Yichen Xu <yichen.x@outlook.com>
2025-04-27 22:56:48 +0200

    add testcase: struct3.scala

--- tests/pos/struct3.scala
+++ tests/pos/struct3.scala
@@ -6,6 +6,9 @@ def main(): Unit =
   val b = Ref(0)
   val p = Pair(a, b)
   val t1 = p.x
-  //val t2 = () => p.x
+  val t2 = () => p.x
   val t3: Ref^{a} = t1
+  val t4: Ref^{p} = t1
+  val t5: Ref^{b} = p.y
+  val t6: Ref^{p} = p.y
 
