e5f946046aa874201cf54e7def8f97fe54a693ff
Yichen Xu <yichen.x@outlook.com>
2025-05-04 22:35:09 +0200

    update typeops

--- src/main/scala/cavia/typechecking/TypeOps.scala
+++ src/main/scala/cavia/typechecking/TypeOps.scala
@@ -298,7 +298,7 @@ object TypePrinter:
     case BaseType.BoolType => "bool"
     case BaseType.ArrayType => "array"
     case BaseType.CharType => "char"
-    case BaseType.BreakType => "Break"
+    // case BaseType.BreakType => "Break"
   def showFunctionType(params: List[Binder], result: Type, cs: Option[CaptureSet] = None, isType: Boolean = false)(using ctx: TypeChecker.Context) =
     def showParams(params: List[Binder])(using ctx: TypeChecker.Context): List[String] = params match
       case Nil => Nil
@@ -643,10 +643,10 @@ object PrimArrayType:
     case Type.AppliedType(Type.Base(BaseType.ArrayType), (elemType: Type) :: Nil) => Some(elemType)
     case _ => None
 
-object BreakCapabilityType:
-  def unapply(tpe: Type): Option[Type] = tpe match
-    case Type.AppliedType(Type.Base(BaseType.BreakType), (returnType: Type) :: Nil) => Some(returnType)
-    case _ => None
+// object BreakCapabilityType:
+//   def unapply(tpe: Type): Option[Type] = tpe match
+//     case Type.AppliedType(Type.Base(BaseType.BreakType), (returnType: Type) :: Nil) => Some(returnType)
+//     case _ => None
 
 object AppliedStructType:
   def unapply(tpe: Type): Option[(StructSymbol, List[Type | CaptureSet])] = tpe match
