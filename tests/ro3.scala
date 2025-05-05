32d64aa11ce98a84f53e5b22529c2b9b5adead9b
Yichen Xu <yichen.x@outlook.com>
2025-05-05 14:14:25 +0200

    tests: enrich the test

--- tests/ro3.scala
+++ tests/ro3.scala
@@ -2,6 +2,7 @@ struct Ref(var data: i32)
 def get(x: Ref^ro): Unit = x.data
 def set(x: Ref^): Unit = x.data = 42
 def par(op1: () => Unit, op2: () => Unit): Unit = ()
+def kill(consume x: Ref^): Unit = ()
 def main(): Unit =
   val a = Ref(0)
   val b = Ref(0)
@@ -11,3 +12,8 @@ def main(): Unit =
   par(() => set(a), () => get(b))  // of course ok
   par(() => get(a), () => set(b))  // of course ok
   par(() => set(a), () => set(b))  // of course ok
+  kill(a)
+  //get(a) // no
+  get(b) // ok
+  set(b) // ok
+  //set(a)  // no
