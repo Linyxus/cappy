fc52a4f1abe647c6b47050e3b07065d2a12e58ac
Yichen Xu <yichen.x@outlook.com>
2025-05-11 15:13:59 +0200

    add a benchmark

--- tests/hellostd.scala
+++ tests/hellostd.scala
@@ -1,6 +1,11 @@
+struct Var(var data: i32)
 def main(): Unit =
   val s: String^ = "hello, world"
   putStrLn(s)
   putStrLn("This is hello from Cavia!!")
-  //val s1: String^ = "hello, ".concat(s)
+  putStrLn("hello, ".concat("world !!"))
+  val sum = Var(0)
+  val elapsed = benchmark(() => (0.until(100000)).iterate((i: i32) => sum.data = sum.data + i))
+  putStrLn("Time elapsed (ms):")
+  #i32println(elapsed)
   ()
