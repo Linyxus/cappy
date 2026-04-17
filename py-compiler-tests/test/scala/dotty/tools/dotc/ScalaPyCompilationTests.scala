76b062033b59e1bb22196d3a81a1108abb3195bd
Yichen Xu <yichen.x@outlook.com>
2026-04-17 16:09:10 +0200

    Collapse test file hierarchy

--- py-compiler-tests/test/scala/dotty/tools/dotc/ScalaPyCompilationTests.scala
+++ py-compiler-tests/test/scala/dotty/tools/dotc/ScalaPyCompilationTests.scala
@@ -25,8 +25,6 @@ class ScalaPyCompilationTests:
     cleanScalaPyOutput(testGroup)
     aggregateTests(
       compileFilesInDir("tests/pos-py", scalaPyOptions),
-      compileFilesInDir("tests/pos-py/javalib/markers", scalaPyOptions),
-      compileFilesInDir("tests/pos-py/javalib/function", scalaPyOptions),
     ).checkRuns()
 
   @Test def negScalaPy: Unit =
