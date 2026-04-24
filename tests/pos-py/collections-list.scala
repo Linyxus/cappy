98af195bdac8bd809d8cb32697d1d180fa56f3e2
Yichen Xu <yichen.x@outlook.com>
2026-04-24 03:55:49 +0200

    New test

---
+++ tests/pos-py/collections-list.scala
@@ -0,0 +1,10 @@
+@main def collectionsList(): Unit =
+  val xs = List(0, 1, 2)
+  val ys = xs.reverse
+  println("reverse:" + ys.head + ":" + ys.tail.head + ":" + ys.tail.tail.head + ":" + ys.tail.tail.tail.isEmpty)
+
+  val nil: List[Int] = Nil
+  println("nil:" + nil.isEmpty + ":" + (nil eq Nil))
+
+  val singleton = 3 :: Nil
+  println("singleton:" + singleton.tail.isEmpty + ":" + singleton.reverse.head)
